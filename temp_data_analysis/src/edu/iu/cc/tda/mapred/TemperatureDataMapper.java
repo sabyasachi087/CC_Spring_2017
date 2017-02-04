8a44ee20416345f50dd9f0b9561b887d0d937f6f
Abhijit Thakre <abhijit.thakre@gmail.com>
2017-02-04 11:18:35 +0530

    Changes done for hadoop version 1.2

--- temp_data_analysis/src/edu/iu/cc/tda/mapred/TemperatureDataMapper.java
+++ temp_data_analysis/src/edu/iu/cc/tda/mapred/TemperatureDataMapper.java
@@ -6,14 +6,13 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.log4j.Logger;
 
 import edu.iu.cc.tda.constants.TDAConstants;
 
 public class TemperatureDataMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
 
-	private static final Logger LOGGER = Logger.getLogger(TemperatureDataMapper.class);
-
+	
+	
 	@Override
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
@@ -21,7 +20,7 @@ public class TemperatureDataMapper extends Mapper<LongWritable, Text, Text, Doub
 			Double temp = new Double(value.toString());
 			context.write(new Text(TDAConstants.KEY_TEMP_DATA), new DoubleWritable(temp));
 		} catch (NumberFormatException nfe) {
-			LOGGER.error("Invalid text data " + value.toString() + " . Ignoring and moving ahead ... ");
+			System.err.println("Invalid text data " + value.toString() + " . Ignoring and moving ahead ... ");
 		}
 
 	}
